c00610b90ed6155a72878ba6cbea9761fe33333f
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-06-30 22:42:10 +0400

    fix(layout-auth-control-flow): replace structural directives with @if

--- src/app/core/layout/main-layout.component.ts
+++ src/app/core/layout/main-layout.component.ts
@@ -2,13 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { AuthService } from '../auth/services/auth.service';
-import { IsAdminDirective } from '../auth/directives/is-admin.directive';
-import { IsAuthenticatedDirective } from '../auth/directives/is-authenticated.directive';
 import { ThemeService } from '../theme/theme.service';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet, IsAdminDirective, IsAuthenticatedDirective],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
