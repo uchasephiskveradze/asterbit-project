7eca63124986759b95f5cea859b5cb1d6c14bf5e
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-07-01 18:17:22 +0400

    fix(app-config-deprecations): replace deprecated bootstrap providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,11 @@
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, inject, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -9,26 +14,14 @@ import { GlobalErrorHandler } from './core/error-handler';
 import { httpErrorInterceptor } from './core/interceptors/http-error.interceptor';
 import { ThemeService } from './core/theme/theme.service';
 
-function initializeTheme(): () => void {
-  return () => {
-    inject(ThemeService);
-  };
-}
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(
-      withFetch(),
-      withInterceptors([authInterceptor, httpErrorInterceptor]),
-    ),
+    provideHttpClient(withInterceptors([authInterceptor, httpErrorInterceptor])),
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
-    provideAnimationsAsync(),
     provideRouter(routes, withComponentInputBinding()),
-    {
-      provide: APP_INITIALIZER,
-      multi: true,
-      useFactory: initializeTheme,
-    },
+    provideAppInitializer(() => {
+      inject(ThemeService);
+    }),
   ],
 };
